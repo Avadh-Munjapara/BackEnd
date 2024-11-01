0752a9d69f16c729bd6ae2c3ae76a658fd4bcb76
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2024-11-01 13:46:44 +0530

    login controller created

--- autNAuthZ/controllers/auth.js
+++ autNAuthZ/controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt=require('bcrypt');
-
+const jwt=require('jsonwebtoken');
 const user=require('../models/userModel')
+require('dotenv').config();
 exports.signUpController=async (req,res)=>{
     const {name,email,password}=req.body;
     try{
@@ -34,4 +35,43 @@ exports.signUpController=async (req,res)=>{
             success:fasle
         })
     }
+}
+
+
+exports.loginController=async (req,res)=>{
+    try{
+        const {email,password}=req.body;
+        if(!email||!password){
+            return res.status(400).json({
+                success:false,
+                message:"email or password is not provided"
+            })
+        }
+        const existedUser=await user.findOne({email});
+        if(!existedUser){
+            return res.status(404).json({
+                success:false,
+                message:"not registerd user"
+            })
+        }
+    
+        if(await bcrypt.compare(password,existedUser.password)){
+    
+            const payload={
+                email,id:existedUser._id,role:"student"
+            }
+            const token=jwt.sign(payload,process.env.JWT_TOKEN);
+            return res.status(200).json({
+                success:true,
+                message:"login successful",
+                token
+            })
+        }
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            message:'error while login'
+        })
+    }
+   
 }
--- autNAuthZ/routes/user.js
+++ autNAuthZ/routes/user.js
@@ -1,5 +1,6 @@
-const { signUpController } = require('../controllers/Auth');
+const { signUpController,loginController } = require('../controllers/Auth');
 const express=require('express');
 const router=express.Router();
 router.post('/signUp',signUpController);
+router.get('/login',loginController);
 module.exports=router;
